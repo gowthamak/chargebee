58ad666c7577cd21c22dfc6c0f76c50a73115b77
gowthamak <gowtham.k434@gmail.com>
2016-07-10 11:54:23 +0530

    Update child.c

--- child.c
+++ child.c
@@ -6,8 +6,14 @@ int a,n,i,j;
 int count=0;
 printf("\n Enter value of a=");
 scanf("%d",&a);
+if(a==0)
+{
+printf("\n no child is present");
+}
+else
+{
 n=2*a;/* number of child*/
-printf("\n the pair of childrens")
+printf("\n the pair of childrens");
 for(i=1;i<n;i++)
 {
 for(j=i+1;j<=n;j++)
@@ -17,5 +23,5 @@ count++;
 }
 }
 printf("No of paires =%d",count);
-
+}
 }
